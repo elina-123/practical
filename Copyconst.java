767f8dd1f7ca35aedd9fae6fac5e508aa4ceec5a
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-28 09:55:03 +0530

    Add files via upload

---
+++ Copyconst.java
@@ -0,0 +1,32 @@
+package pro1;
+
+public class Copyconst {
+
+	//variable along with data type
+		int age;
+		String name;
+		Copyconst(int a,String n){//using constructor
+			age=a;
+			name=n;
+		}
+		/*Copyconst()//using default constructor
+		{}*/
+	//constructor to initialize another object
+	Copyconst(Copyconst c)//using constructor
+	  {
+		age=c.age;
+		name=c.name;}
+		void show() {
+			System.out.println(age+" "+name);
+		}
+		public static void main(String[] args) {
+			Copyconst c1=new Copyconst(22,"Akd");//using constructor
+			Copyconst c2=new Copyconst(c1);//using constructor
+			/*Copyconst c2=new Copyconst();//using default constructor
+			c2.age=c1.age;
+			c2.name=c1.name;*/
+			c1.show();
+			c2.show();
+		}
+
+}
