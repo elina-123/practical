d30cbadfa4f080a271ce46e80544691c32463dcd
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-10-10 19:32:03 +0530

    Add files via upload

---
+++ Inheritance1.java
@@ -0,0 +1,22 @@
+package pro1;
+
+//Single Inheritance
+class vehicle{//parent class A
+	void running() {
+		System.out.println("running mode is on");
+	}
+}
+	class bike extends vehicle{//child class b
+		void run() {
+			System.out.println("Bike run mode on");
+		}
+	}
+
+public class Inheritance1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bike b1=new bike();//object of child class
+		b1.run();//child class method
+		b1.running();//parent class
+	}}
