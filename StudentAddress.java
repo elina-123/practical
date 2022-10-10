4c242f67e34d37cdc7c17f1d664bef8bd23e899c
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-10-10 20:08:05 +0530

    Add files via upload

---
+++ StudentAddress.java
@@ -0,0 +1,12 @@
+package pro1;
+
+public class StudentAddress { //class 1
+
+	String area,city,dist;
+	public StudentAddress(String area,String city,String dist) {
+		this.city=city;
+		this.area=area;
+		this.dist=dist;
+	
+	}
+}
