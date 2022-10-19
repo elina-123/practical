70820e637621f42fbd5e254c938cbf38c3fc6940
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-10-19 20:04:50 +0530

    Add files via upload

---
+++ AbstractMethod.java
@@ -0,0 +1,28 @@
+
+//class A( should be abstract class)
+//abstract void show()//abstract method
+package pro1;
+abstract class Testabstract{//abstract method
+	abstract void display();//abstract method
+   void show() {//non abstract method
+	System.out.println("non abstract method invoked");   
+   }
+}
+ class Demo1  extends Testabstract{//1st child class
+void display() {//non abstract method with same name
+	System.out.println("Demo1 method invoked");
+}}
+ class Demo2  extends Testabstract{//2st child class
+  void display() {
+	  System.out.println("Demo2 method invoked");
+  }}
+ class AbstractMethod{
+public static void main(String[] args) {
+	Testabstract obj =new Demo1();
+	Testabstract obj1 =new Demo2();
+	obj.display();
+	obj1.display();
+	obj.show();
+	}
+
+}
