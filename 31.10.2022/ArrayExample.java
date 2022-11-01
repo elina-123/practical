4350da91c628d9b1d4848c4eb7e65e0ef2c415cb
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-01 10:14:45 +0530

    Add files via upload

---
+++ 31.10.2022/ArrayExample.java
@@ -0,0 +1,21 @@
+package pro1;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+		//declaration
+		String[] names= {"elina","abinash","suchitra","akshaya","suchitra","lucy"};
+		//for loop
+		for(int i=0;i<names.length;i++) {
+			System.out.println(names[i]);
+		}
+System.out.println();
+//declaration
+int [] phone= {123,456,789,955,333,555,111,2345,7890};
+//for each loop
+for(int n:phone) {
+	System.out.println(n);
+}
+	}
+
+
+}
