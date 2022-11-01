6a4bb8908a11f9bb914933ce9960315204ac4762
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-01 10:18:08 +0530

    Add files via upload

---
+++ 31.10.2022/ArrayMethod.java
@@ -0,0 +1,16 @@
+package pro1;
+
+public class ArrayMethod {
+	static int[] get() {
+		return new int[] {10,20,30};
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=get();
+for (int i=0; i<arr.length;i++)
+	System.out.println(arr[i]);
+	}
+
+
+}
