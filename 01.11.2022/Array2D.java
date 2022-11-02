59b19dbc7d8b6da1b7366ed1af0e933dcc169dc0
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-02 10:04:49 +0530

    Add files via upload

---
+++ 01.11.2022/Array2D.java
@@ -0,0 +1,13 @@
+package pro1;
+//Multidimensional array
+public class Array2D {
+public static void main(String[] args) {
+	//declaring and initializing 2D array
+	int arr[][]= {{2,5,6},{4,6,8}};
+	for(int i=0;i<2;i++) {//for printing row
+		for(int j=0;j<3;j++) {
+		System.out.print(arr[i][j]+" ");	
+		}
+		System.out.println();
+	}
+}}
