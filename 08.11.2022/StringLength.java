ecded97357192b1c3e23c28adfef97e8be602dbd
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-09 09:54:44 +0530

    Add files via upload

---
+++ 08.11.2022/StringLength.java
@@ -0,0 +1,11 @@
+package pro1;
+
+public class StringLength {
+public static void main(String[] args) {
+String str = "Anudip Foundation";	
+//get the length of str.
+int len = str.length();
+System.out.println("The string length of " +str+ " is: "+len);
+	}
+
+}
