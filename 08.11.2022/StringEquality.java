20671bce222ba381d18ac8c6e33593818f5af1a1
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-09 09:50:11 +0530

    Add files via upload

---
+++ 08.11.2022/StringEquality.java
@@ -0,0 +1,22 @@
+package pro1;
+import java.util.Scanner;
+//check equality by using equal()method
+public class StringEquality {
+
+	public static void main(String[] args) {
+	String str1, str2;//
+	Scanner ab = new Scanner(System.in);
+	System.out.println("Enter first String");
+	str1 = ab.nextLine();
+	
+	System.out.println("Enter second String");
+	str2 = ab.nextLine();
+	//comparing two input string
+	if (str1.equals(str2))
+		System.out.println("Equal Strings");
+	else
+		System.out.println("UnEqual Strings");
+
+	}
+
+}
